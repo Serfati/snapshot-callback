85a0e5f64c8f9e240c4962ab6c0ab9d455883b74
Avihai <avihaipP@gmail.com>
2021-06-13 10:29:48 +0300

    change optimizer

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,12 +1,13 @@
 from keras.layers import *
 from keras.models import Model
+from keras.metrics import AUC
 
 class Baseline():
     def __init__(self,x_in, x_out):
         self.x_in = x_in
         self.x_out = x_out
 
-    def baseline(self, optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy']):
+    def baseline(self, optimizer='sdg', loss='categorical_crossentropy', metrics=['accuracy', 'TruePositives', 'FalsePositives', 'Precision', AUC(), AUC(curve='PR')]):
         
         x = Conv2D(32, (3,3), activation='relu', padding='same', use_bias=False)(self.x_in)
         x = Conv2D(32, (3,3), activation='relu', padding='same', use_bias=False)(x)
